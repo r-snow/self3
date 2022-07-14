d3d729e4a7ea7a5cac7fd5070ec82fe4466ac994
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-07-13 20:52:27 -0700

    adding safe-area provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Self 3.0</Text>
-    </View>
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Self 3.0</Text>
+      </View>
+    </SafeAreaProvider>
   );
 };
 export default App;
